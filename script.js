68b4fd2bd51ab9bacb994cc9273c5113759fe75a
Kevin Lueke <kevinlueke66@gmail.com>
2019-12-03 16:23:14 -0500

    Change cage size.

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let player = document.getElementById('player');
 let square = document.getElementById('square');
 
 const playerPos = [6, 11];
-player.style.left = playerPos[0] + 'rem';
-player.style.top = playerPos[1] + 'rem';
+//player.style.left = playerPos[0] + 'rem';
+//player.style.top = playerPos[1] + 'rem';
 
 const squarePos = [(Math.random() * 95), (Math.random() * 95)];
-square.style.left = squarePos[0] + '%';
-square.style.top = squarePos[1] + '%';
+//square.style.left = squarePos[0] + '%';
+//square.style.top = squarePos[1] + '%';
 
 
