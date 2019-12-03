79ffe0359a82f0e33ba93296418908daef6db28e
Wilnelys Roman <wilnelys11@hotmail.com>
2019-12-03 15:21:30 -0500

    place player at start position

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const cage = document.getElementById('cage');
 let player = document.getElementById('player');
 let square = document.getElementById('square');
+
+const playerPos = [2, 5]
+player.style.left = playerPos[0] + 'rem';
+player.style.top = playerPos[1] + 'rem';
