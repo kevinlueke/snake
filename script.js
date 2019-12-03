33ce06dd87d3134fcc0d71b70b767d2ebd234ca2
Kevin Lueke <kevinlueke66@gmail.com>
2019-12-03 15:46:47 -0500

    Randomize square at start and got rid of border.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const cage = document.getElementById('cage');
+const cage = document.getElementById('body');
 let player = document.getElementById('player');
 let square = document.getElementById('square');
 
-const playerPos = [2, 5]
+const playerPos = [6, 11];
 player.style.left = playerPos[0] + 'rem';
 player.style.top = playerPos[1] + 'rem';
+
+const squarePos = [(Math.random() * 95), (Math.random() * 95)];
+square.style.left = squarePos[0] + '%';
+square.style.top = squarePos[1] + '%';
+
+
