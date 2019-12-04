e7f30fa1717bae93475d17b06cded08d108bfb02
Kevin Lueke <kevinlueke66@gmail.com>
2019-12-03 20:24:13 -0500

    Fix more background and square issues

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const playerPos = [6, 11];
 //player.style.left = playerPos[0] + 'rem';
 //player.style.top = playerPos[1] + 'rem';
 
-const squarePos = [(Math.random() * 95), (Math.random() * 95)];
-//square.style.left = squarePos[0] + '%';
-//square.style.top = squarePos[1] + '%';
+const squarePos = [(Math.round(Math.random() * 100)), (Math.round(Math.random() * 100))];
+square.style.left = squarePos[0] + 'vw';
+square.style.top = squarePos[1] + 'vh';
 
 
