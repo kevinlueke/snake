54dc783dd41c3ba7fb66334d0a6b8ddeabffc095
Wilnelys Roman <wilnelys11@hotmail.com>
2019-12-05 15:23:07 -0500

    Make player move

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const cage = document.getElementById('cage');
-let player = document.getElementById('player');
+let player = document.getElementById('snake');
 let square = document.getElementById('square');
 let size = 2;
 let c = 98/size;
@@ -17,6 +17,24 @@ square.style.width = size + '%';
 
 square.style.left = squarePos[0] + '%';
 square.style.top = squarePos[1] + '%';
-square.style.right = squarePos[0] + '%';
-square.style.bottom = squarePos[1] + '%';
-  
+
+const move = (event) => {
+ if (event.key === 'ArrowRight') {
+   playerPos[0] += 2;
+   player.style.left = playerPos[0] + '%';
+   event.preventDefault();
+ } else if (event.key === 'ArrowLeft') {
+   playerPos[0] -= 2;
+   player.style.left = playerPos[0] + '%';
+   event.preventDefault();
+ } else if (event.key === 'ArrowUp') {
+   playerPos[1] -= 2;
+   player.style.top = playerPos[1] + '%';
+   event.preventDefault();
+ } else if (event.key === 'ArrowDown') {
+   playerPos[1] += 2;
+   player.style.top = playerPos[1] + '%';
+   event.preventDefault();
+ }
+};
+document.addEventListener('keydown', move);
