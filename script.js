7d6dd4c2e0aacbf4f3aace3ba18438e1c9d4d6ae
Kevin Lueke <kevinlueke66@gmail.com>
2019-12-05 15:27:32 -0500

    Start allowing square to be obtained.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const cage = document.getElementById('cage');
-let player = document.getElementById('player');
+let player = document.getElementById('snake');
 let square = document.getElementById('square');
 let size = 2;
 let c = 98/size;
@@ -17,6 +17,27 @@ square.style.width = size + '%';
 
 square.style.left = squarePos[0] + '%';
 square.style.top = squarePos[1] + '%';
-square.style.right = squarePos[0] + '%';
-square.style.bottom = squarePos[1] + '%';
   
+if ((player.style.left === square.style.left /*player arr index 01 is equal to square indes 01.*/) && (player.style.top === square.style.top )) {
+  //square becomes snake and removes square id
+  //takes coordinates inside an array from square and puts it into snake array
+  //when square randomly regenerates it adds coordinates back to array
+}
+const squareArr = [squarePos[0], squarePos[1]];
+playerPos.reverse();
+while (squareArr.length > 0) {
+  playerPos.push(squareArr.pop());
+}
+playerPos.reverse();
+//remove id square
+square.classList.add('snakeBody');
+square.removeAttribute(square);
+//creat new div with id square
+let newSquare = document.createElement('div');
+newSquare.setAttribute('id', 'square');
+main.appendChild(newSquare);
+//add class player
+console.log(playerPos);
+//move block
+ 
+
