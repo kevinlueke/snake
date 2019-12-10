f7376b72bf1c1389cd8a2755e12eab730ed7825b
Wilnelys Roman <wilnelys11@hotmail.com>
2019-12-10 14:02:25 -0500

    fix direction bug
    player unable to move opposite direction fast & glitch game

--- script.js
+++ script.js
@@ -1,152 +1,165 @@
 const cage = document.getElementById('cage');
 
 let playerPos,
-    squarePos,
-    dir,
-    scores = 0,
-    gameScore = document.getElementById('score'),
-    square = document.getElementById('square'),
-    player = document.querySelector('.snakeBody'),
-    divEl = document.createElement('div'),
-    size = 4,
-    playerLoc = [],
-    i = 0,
-    j = 0,
-    c = 98/size;
+  squarePos,
+  dir,
+  scores = 0,
+  gameScore = document.getElementById('score'),
+  square = document.getElementById('square'),
+  player = document.querySelector('.snakeBody'),
+  divEl = document.createElement('div'),
+  size = 4,
+  playerLoc = [],
+  i = 0,
+  j = 0,
+  c = 98 / size,
+  k = true;
 
-//Directions the snake moves 
+//Directions the snake moves
 const move = (event) => {
-  if (event.key === 'ArrowRight' && dir !== 'left') {
-    j = 0
-    event.preventDefault();
-    dir = 'right';
-    let timeout = value => {
-      setTimeout(() => {
+  if (k === true) {
+    if (event.key === 'ArrowRight' && dir !== 'left') {
+      k = false;
+      j = 0
+      event.preventDefault();
+      dir = 'right';
+      let timeout = value => {
+        setTimeout(() => {
 
 
-       if (dir === 'right') {
-         moreSnake();
-         playerPos[0][0] += size;
-        // playerLoc.push(playerPos);
-         // updating snakes position
-         player.style.left = playerPos[0][0] + '%';
-         player.style.height = size + '%';
-         player.style.width = size + '%';
-         player.style.top = playerPos[0][1] + '%';
-         timeout(value);
-        }
-      }, 100)
-   }
-     if (i === 0) {
-         ++i;
-         timeout(playerPos[0][0]);
-     }
-
-  } else if (event.key === 'ArrowLeft' && dir !== 'right') {
-    j = 0;
-    event.preventDefault();
-
-    dir = 'left';
-
-    let timeout = value => {
-      setTimeout(() => {
-
-
-        if (dir === 'left') {
+          if (dir === 'right') {
             moreSnake();
-          playerPos[0][0] -= size;
-         //playerLoc.push(playerPos);
-          player.style.left = playerPos[0][0] + '%';
-          player.style.top = playerPos[0][1] + '%';
-          player.style.height = size + '%';
-          player.style.width = size + '%';
-          timeout(value);
+            playerPos[0][0] += size;
+            // playerLoc.push(playerPos);
+            // updating snakes position
+            player.style.left = playerPos[0][0] + '%';
+            player.style.height = size + '%';
+            player.style.width = size + '%';
+            player.style.top = playerPos[0][1] + '%';
+            k = true;
+            timeout(value);
+          }
+        }, 100)
+      }
+      if (i === 0) {
+        ++i;
+        timeout(playerPos[0][0]);
+      }
 
-        }
-      }, 100)
+    } else if (event.key === 'ArrowLeft' && dir !== 'right') {
+      k = false;
+      j = 0;
+      event.preventDefault();
+
+      dir = 'left';
+
+      let timeout = value => {
+        setTimeout(() => {
+
+
+          if (dir === 'left') {
+            moreSnake();
+            playerPos[0][0] -= size;
+            //playerLoc.push(playerPos);
+            player.style.left = playerPos[0][0] + '%';
+            player.style.top = playerPos[0][1] + '%';
+            player.style.height = size + '%';
+            player.style.width = size + '%';
+            k = true;
+            timeout(value);
+
+          }
+        }, 100)
+      }
+
+      if (i === 0) {
+        ++i;
+        timeout(playerPos[0][0]);
+      }
+
+    } else if (event.key === 'ArrowUp' && dir !== 'down') {
+      k = false;
+      i = 0;
+      event.preventDefault();
+
+      dir = 'up';
+      let timeout = value => {
+        setTimeout(() => {
+          if (dir === 'up') {
+            moreSnake();
+            playerPos[0][1] -= size;
+            // playerLoc.push(playerPos);
+            player.style.top = playerPos[0][1] + '%';
+            player.style.left = playerPos[0][0] + '%';
+            player.style.height = size + '%';
+            player.style.width = size + '%';
+            k = true;
+            timeout(value);
+          }
+        }, 100)
+      }
+
+      if (j === 0) {
+        ++j;
+        timeout(playerPos[0][1]);
+      }
+
+    } else if (event.key === 'ArrowDown' && dir !== 'up') {
+      k = false;
+      i = 0;
+      event.preventDefault();
+      dir = 'down';
+      let timeout = value => {
+        setTimeout(() => {
+
+          if (dir === 'down') {
+            moreSnake();
+            playerPos[0][1] += size;
+            // playerLoc.push(player.style.left);
+            player.style.top = playerPos[0][1] + '%';
+            player.style.left = playerPos[0][0] + '%';
+            player.style.height = size + '%';
+            player.style.width = size + '%';
+            k = true;
+            timeout(value);
+          }
+        }, 100)
+      }
+
+      if (j === 0) {
+        ++j;
+        timeout(playerPos[0][1]);
+      }
     }
-   
-     if (i === 0) {
-         ++i;
-         timeout(playerPos[0][0]);
-     }
-
-
-
-  } else if (event.key === 'ArrowUp' && dir !== 'down') {
-    i = 0;
-    event.preventDefault();
-
-    dir = 'up';
-    let timeout = value => {
-      setTimeout(() => {
-        if (dir === 'up') {
-          moreSnake();
-          playerPos[0][1] -= size;
-         // playerLoc.push(playerPos);
-          player.style.top = playerPos[0][1] + '%';
-          player.style.left = playerPos[0][0] + '%';
-          player.style.height = size + '%';
-          player.style.width = size + '%';
-          timeout(value);
-        }
-      }, 100)
-    }
-    
-     if (j === 0) {
-         ++j;
-         timeout(playerPos[0][1]);
-     }
-
-  } else if (event.key === 'ArrowDown' && dir !== 'up') {
-    i = 0;
-    event.preventDefault();
-    dir = 'down';
-    let timeout = value => {
-      setTimeout(() => {
-
-        if (dir === 'down') {
-          moreSnake();
-          playerPos[0][1] += size;
-         // playerLoc.push(player.style.left);
-          player.style.top = playerPos[0][1] + '%';
-          player.style.left = playerPos[0][0] + '%';
-          player.style.height = size + '%';
-          player.style.width = size + '%';
-          timeout(value);
-        }
-      }, 100)
-    }
-    
-     if (j === 0) {
-         ++j;
-         timeout(playerPos[0][1]);
-     }
-  }
     //console.log(playerLoc);
+  }
 }
 gameStart = () => {
-    gameScore.textContent = `Score: ${scores}`;
-//Creates a div for snake and selects it, styles it
-    cage.appendChild(divEl);
-    divEl.className = 'snakeBody';
-    player = document.querySelector('.snakeBody');
-    playerPos = [ [size, size] ];
-    //playerLoc.unshift(player.style.top);
-    //playerLoc.unshift(player.style.left);
-    player.style.height = size + '%';
-    player.style.width = size + '%';
+  gameScore.textContent = `Score: ${scores}`;
+  //Creates a div for snake and selects it, styles it
+  cage.appendChild(divEl);
+  divEl.className = 'snakeBody';
+  player = document.querySelector('.snakeBody');
+  playerPos = [
+    [size, size]
+  ];
+  //playerLoc.unshift(player.style.top);
+  //playerLoc.unshift(player.style.left);
+  player.style.height = size + '%';
+  player.style.width = size + '%';
 
-    player.style.left = playerPos[0][0] + '%';
-    player.style.top = playerPos[0][1] + '%';
-    squarePos = [ [(Math.floor(Math.random() * c)) * size, (Math.floor(Math.random() * c)) * size] ];
-    square.style.height = size + '%';
-    square.style.width = size + '%';
+  player.style.left = playerPos[0][0] + '%';
+  player.style.top = playerPos[0][1] + '%';
+  squarePos = [
+    [(Math.floor(Math.random() * c)) * size, (Math.floor(Math.random() * c)) * size]
+  ];
+  square.style.height = size + '%';
+  square.style.width = size + '%';
 
-    square.style.left = squarePos[0][0] + '%';
-    square.style.top = squarePos[0][1] + '%';
-    document.addEventListener('keydown', move);
-    }
+  square.style.left = squarePos[0][0] + '%';
+  square.style.top = squarePos[0][1] + '%';
+  document.addEventListener('keydown', move);
+}
 
 gameOver = () => {
 
@@ -161,18 +174,20 @@ const moreSnake = () => {
     gameScore.textContent = `Score: ${++scores}`;
     ++scores;
     gameScore.textContent = `Score: ${scores}`;
-//updates squares random position
-    squarePos = [ [(Math.floor(Math.random() * c)) * size, (Math.floor(Math.random() * c)) * size] ];
+    //updates squares random position
+    squarePos = [
+      [(Math.floor(Math.random() * c)) * size, (Math.floor(Math.random() * c)) * size]
+    ];
     square.style.left = squarePos[0][0] + '%';
     square.style.top = squarePos[0][1] + '%';
     //playerLoc.unshift(player.style.top);
     //playerLoc.unshift(player.style.left);
-    
-// creates a div element and appends it to the cage. Gives it the class snakeBody
+
+    // creates a div element and appends it to the cage. Gives it the class snakeBody
     cage.append(divEl);
     divEl.className = 'snakeBody';
     player = document.querySelector('.snakeBody');
-//fix empty block bug
+    //fix empty block bug
     if (dir === 'right') {
       playerPos[0][0] -= size;
     } else if (dir === 'left') {
@@ -183,13 +198,13 @@ const moreSnake = () => {
       playerPos[0][1] -= size;
     }
   } else {
-   // playerLoc.unshift(player.style.top);
-   // playerLoc.unshift(player.style.left);
-   // playerLoc.pop();
-   // playerLoc.pop();
+    // playerLoc.unshift(player.style.top);
+    // playerLoc.unshift(player.style.left);
+    // playerLoc.pop();
+    // playerLoc.pop();
 
-// Takes the already existing div and moves it to the end
-// It will then update the current player and updates div
+    // Takes the already existing div and moves it to the end
+    // It will then update the current player and updates div
     console.log(playerPos);
     cage.append(player);
     player = document.querySelector('.snakeBody');
