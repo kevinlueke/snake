59bd6ea4f5ac56c65ba7c09811f1a626adb75840
Wilnelys Roman <wilnelys11@hotmail.com>
2019-12-12 16:24:32 -0500

    fix forfeit bug

--- script.js
+++ script.js
@@ -1,57 +1,75 @@
 const cage = document.getElementById('cage');
 let playerHead,
-  playerLoc = [],
+  playerLoc,
   squarePos,
   dir,
+  lose = false,
   scores = 0,
   gameScore = document.getElementById('score'),
   square = document.getElementById('square'),
   player = document.querySelector('.snakeBody'),
   divEl = document.createElement('div'),
+  reset = document.getElementById('reset'),
   size = 4,
-  i = 0,
+  i,
   c = 98 / size,
-  k = true;
+  deBounce = true;
 
 const timeout = value => {
   setTimeout(() => {
-      ++i;
-      moreSnake();
-      player.style.top = playerHead[1] + '%';
-      player.style.left = playerHead[0] + '%';
-      player.style.height = size + '%';
-      player.style.width = size + '%';
-      k = true;
-      if (dir !== null) timeout(value);
+    ++i;
+    moreSnake();
+    player.style.top = playerHead[1] + '%';
+    player.style.left = playerHead[0] + '%';
+    player.style.height = size + '%';
+    player.style.width = size + '%';
+    deBounce = true;
+    if (lose === false) timeout(value);
   }, 100)
 }
 //Directions the snake moves
 const move = (event) => {
-  if (k) {
-    if (typeof(dir) === 'string') {
-      event.preventDefault();
-      k = false;
-    }
+  if (deBounce) {
     if (event.key === 'ArrowRight' && dir !== 'left') {
       dir = 'right';
+      lose = false;
+      deBounce = false;
       i === 0 ? timeout(playerHead[0]) : i = 0;
+        event.preventDefault();
     } else if (event.key === 'ArrowLeft' && dir !== 'right') {
       dir = 'left';
+      lose = false;
+      deBounce = false;
       i === 0 ? timeout(playerHead[0]) : i = 0;
+        event.preventDefault();
     } else if (event.key === 'ArrowUp' && dir !== 'down') {
       dir = 'up';
+      lose = false;
+      deBounce = false;
       i === 0 ? timeout(playerHead[1]) : i = 0;
+        event.preventDefault();
     } else if (event.key === 'ArrowDown' && dir !== 'up') {
       dir = 'down';
+      lose = false;
+      deBounce = false;
       i === 0 ? timeout(playerHead[1]) : i = 0;
+        event.preventDefault();
     }
   }
 }
 gameStart = () => {
+  lose = false;
+  i = 0;
+  playerLoc = [];
+  scores = 0;
   gameScore.textContent = `Score: ${scores}`;
   //Creates a div for snake and selects it, styles it
   cage.appendChild(divEl);
   divEl.className = 'snakeBody';
+  let squareEl = document.createElement('div');
+  cage.appendChild(squareEl);
+  squareEl.setAttribute('id', 'square');
+  square = document.getElementById('square');
   player = document.querySelector('.snakeBody');
   playerHead = [size, size];
   playerLoc.push(Array.from(playerHead));
@@ -60,21 +78,25 @@ gameStart = () => {
 
   player.style.left = playerHead[0] + '%';
   player.style.top = playerHead[1] + '%';
-  squarePos = [(Math.floor(Math.random() * c)) * size, (Math.floor(Math.random() * c)) * size];
+  squarePos = [[(Math.floor(Math.random() * c)) * size, (Math.floor(Math.random() * c)) * size]];
   square.style.height = size + '%';
   square.style.width = size + '%';
 
-  square.style.left = squarePos[0] + '%';
-  square.style.top = squarePos[1] + '%';
-  document.addEventListener('keydown', move);
+  square.style.left = squarePos[0][0] + '%';
+  square.style.top = squarePos[0][1] + '%';
+  window.addEventListener('keydown', move);
 }
 
 gameOver = () => {
-    dir = null;
-    playerLoc = [];
-    document.removeEventListener('keydown', move);
-    //cage.append(divEl);
-    //divEl.className = 'dead';
+  lose = true;
+  dir = null;
+  while (cage.firstChild) {
+      cage.firstChild.remove();
+    }
+  window.removeEventListener('keydown', move);
+  gameStart();
+  //cage.append(divEl);
+  //divEl.className = 'dead';
 }
 
 gameStart();
@@ -83,9 +105,10 @@ const moreSnake = () => {
   if ((player.style.left === square.style.left) && (player.style.top === square.style.top)) {
     gameScore.textContent = `Score: ${++scores}`;
     //updates squares random position
-    squarePos = [(Math.floor(Math.random() * c)) * size, (Math.floor(Math.random() * c)) * size];
-    square.style.left = squarePos[0] + '%';
-    square.style.top = squarePos[1] + '%';
+    squarePos = [[(Math.floor(Math.random() * c)) * size, (Math.floor(Math.random() * c)) * size]];
+    if (checkArray(squarePos[0], playerLoc)) squarePos = [[(Math.floor(Math.random() * c)) * size, (Math.floor(Math.random() * c)) * size]];
+    square.style.left = squarePos[0][0] + '%';
+    square.style.top = squarePos[0][1] + '%';
     playerLoc.unshift(Array.from(playerHead));
     // creates a div element and appends it to the cage. Gives it the class snakeBody
     cage.append(divEl);
@@ -102,11 +125,11 @@ const moreSnake = () => {
     if (dir === 'down') playerHead[1] += size;
     playerLoc.unshift(Array.from(playerHead));
     playerLoc.pop();
-       if (checkArray(playerLoc[0], playerLoc.slice(1)) ||
-        playerHead[0] < 0 ||
-        playerHead[1] < 0 ||
-        playerHead[0] > 96 ||
-        playerHead[1] > 96) gameOver();
+    if (checkArray(playerLoc[0], playerLoc.slice(1)) ||
+      playerHead[0] < 0 ||
+      playerHead[1] < 0 ||
+      playerHead[0] > 96 ||
+      playerHead[1] > 96) gameOver();
   }
 }
 
@@ -116,5 +139,7 @@ checkArray = (a, arr) => {
       return true;
     }
   }
-  return false;  
+  return false;
 }
+
+reset.addEventListener ("click", gameOver);
